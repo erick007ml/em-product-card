bbce9149f25c34aa993a978a694946cc97f3c592
Erick Mauricio <coartac.erick.mamani@gmail.com>
2024-11-15 18:42:00 -0500

    Version 0.0.1 lista

---
+++ test/component/ProductCard.test.tsx
@@ -0,0 +1,40 @@
+import React, { act } from 'react';
+import renderer from 'react-test-renderer';
+import { ProductCard } from '../../src/components';
+
+import { product1 } from '../data/products';
+
+describe('ProductCard', () => {
+  test('debe de mostrar el componente correctamente', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product1}>
+        {() => (
+          <>
+            <h1>Hola </h1>
+          </>
+        )}
+      </ProductCard>
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+  test('debe de incrementar el contador', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product1}>
+        {({ count, increaseBy }) => (
+          <>
+            <h1>Product Card</h1>
+            <span>{count}</span>
+            <button onClick={() => increaseBy(1)}>Incrementar</button>
+          </>
+        )}
+      </ProductCard>
+    );
+    let tree = wrapper.toJSON();
+    expect(tree).toMatchSnapshot();
+    act(() => {
+      wrapper.root.findByType('button').props.onClick();
+    });
+    tree = wrapper.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ test/component/ProductTitle.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductCard, ProductTitle } from '../../src/components';
+
+import { product1, product2 } from '../data/products';
+
+describe('ProductTitle', () => {
+  test('debe de mostrar el componente correctamente con el titulo', () => {
+    const wrapper = renderer.create(<ProductTitle title={'Product Title'} />);
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+  test('debe de mostrar el componente con el nombre del producto', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product1}>
+        {() => (
+          <>
+            <ProductTitle />
+          </>
+        )}
+      </ProductCard>
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+  test('debe de mostrar el componente con el nombre del producto', () => {
+    const wrapper = renderer.create(
+      <ProductCard product={product2}>
+        {() => (
+          <>
+            <ProductTitle />
+          </>
+        )}
+      </ProductCard>
+    );
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+});
